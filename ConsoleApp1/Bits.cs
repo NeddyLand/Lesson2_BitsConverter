e11437bd0b04bce3aea569788c83fed2888d89f8
Neddy <Ned@gmail.com>
2024-04-07 11:29:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Bits.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitsConverter
+{
+    class Bits
+    {
+        public byte Value { get; private set; } = 0;
+        public Bits() { }
+        public Bits(byte b) 
+        {
+            this.Value = b;
+        }
+
+        public bool this[int index]
+        {
+            get 
+            {
+                if (index > 7 || index < 0)
+                    return false;
+                return ((Value >> index) & 1) == 1;
+            }
+            set 
+            {
+                if (index > 7 || index < 0)
+                    return;
+                if (value == true)
+                    Value = (byte)(Value | (1 << index));
+                else
+                {
+                    var mask = (byte)(1 << index);
+                    mask = (byte)(0xff ^ mask);
+                    Value = (byte)(Value & mask);
+                }
+            }
+        }
+
+        public static implicit operator Bits(byte b) => new Bits(b);
+        public static implicit operator Bits(long b) => new Bits((byte)b);
+        public static implicit operator Bits(int b) => new Bits((byte)b);
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BitsConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            long b = 20;
+            byte c = 30;
+            var bitsInt = new Bits();
+            var bitsLong = new Bits();
+            var bitsByte = new Bits();
+            bitsInt = a;
+            Console.WriteLine(bitsInt);
+            bitsLong = b;
+            Console.WriteLine(bitsLong);
+            bitsByte = c;
+            Console.WriteLine(bitsByte);
+        }
+    }
+}
